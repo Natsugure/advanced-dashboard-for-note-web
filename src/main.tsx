ddb0bc4dfaea1a4665ce4dd2715a02599652fea8
Natsugure <natsugure5301102@gmail.com>
2026-05-23 10:00:18 +0900

    React RouterとMantineを設定

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router'
+import router from './routers/Router.tsx'
+import '@mantine/core/styles.css'
+import './theme.css'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider>
+      <RouterProvider router={router} />
+    </MantineProvider>
   </StrictMode>,
 )
