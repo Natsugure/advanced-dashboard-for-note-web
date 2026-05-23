693700845e643fbdf1b76c460fd7ee9693e6a55d
Natsugure <natsugure5301102@gmail.com>
2026-05-23 11:22:59 +0900

    Clerkのビルドインサインインコンポーネントを設置

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,9 +1,11 @@
+import { SignIn } from "@clerk/react";
 import { Title } from "@mantine/core";
 
 export function LoginPage() {
   return (
     <>
       <Title order={2}>ログインページ</Title>
+      <SignIn />
     </>
   )
 }
