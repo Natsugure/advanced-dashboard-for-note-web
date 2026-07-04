0ddfd5092b178ae00895104b26da549d02d218c8
Natsugure <natsugure5301102@gmail.com>
2026-07-04 09:15:37 +0900

    feat: noteアカウント未連携ユーザーへの案内オーバーレイを実装

--- src/features/dashboard/pages/DashboardPage.tsx
+++ src/features/dashboard/pages/DashboardPage.tsx
@@ -1,11 +1,65 @@
-import { Container, Tabs, Title } from "@mantine/core"
+import { Anchor, Box, Center, Container, EmptyState, Overlay, Tabs, Text, Title } from "@mantine/core"
 import { TrendTabContent } from "../components/TrendTabContent";
 import { ArticleTabContent } from "../components/ArticleTabContent";
+import { useContext, useEffect, useState } from "react";
+import { Link } from "react-router";
+import { UserContext } from "@/features/auth/contexts/userContext";
+import { useAuth } from "@clerk/react";
+import { RxLinkBreak2 } from "react-icons/rx";
+import { useCurrentUser } from "@/infrastructures/api/hooks/useCurrentUser";
+import { CrxStoreButton } from "@/features/help/components/CrxStoreButton";
 
 export function DashboardPage() {
+  const [userError, setUserError] = useState("")
+  const [isPresentTutorialOverlay, setIsPresentTutorialOverlay] = useState(false)
+  const { setUser, setIsLoading } = useContext(UserContext)
+  const { isLoaded, isSignedIn } = useAuth()
+  const { fetchUser } = useCurrentUser()
+
+  useEffect(() => {
+    const setUserContext = async () => {
+      if (isLoaded && isSignedIn) {
+        setIsLoading(true)
+
+        try {
+          const res = await fetchUser()
+          setUser(res)
+          setIsPresentTutorialOverlay(!res)
+        } catch(e) {
+          console.error(e)
+          setUserError("ユーザー情報の取得に失敗しました")
+        } finally {
+          setIsLoading(false)
+        }
+      }
+    }
+
+    void setUserContext()
+  }, [isLoaded, isSignedIn, setUser])
+
   return (
     <>
       <Container size="lg" mt="xl">
+        <Box pos="relative">
+        {isPresentTutorialOverlay && (
+          <Overlay zIndex={1001} color="#fff" backgroundOpacity={1} radius="sm">
+            <Center h="100%">
+              <EmptyState
+                icon={<RxLinkBreak2 />}
+                title="noteとの連携が完了していません"
+                description="統計情報を閲覧するには、Chrome 拡張機能を使って note の統計情報を取得する必要があります。"
+              >
+                <EmptyState.Actions>
+                  <CrxStoreButton size="md" />
+                </EmptyState.Actions>
+                <EmptyState.Actions>
+                  
+                </EmptyState.Actions>
+                <Text mt="xl">詳しい使い方は、<Anchor component={Link} to="/help" fw={700}>ヘルプページ</Anchor>をご覧ください。</Text>
+              </EmptyState>
+            </Center>
+          </Overlay>
+        )}
         <Title order={2}>ダッシュボード</Title>
         <Tabs defaultValue="trend" mt="md" keepMountedMode="display-none">
           <Tabs.List>
@@ -19,6 +73,7 @@ export function DashboardPage() {
             <ArticleTabContent />
           </Tabs.Panel>
         </Tabs>
+        </Box>
       </Container>
     </>
   )
